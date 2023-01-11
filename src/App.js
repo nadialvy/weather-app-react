28d3145957e36e04df6a48fdfc45dbcbf748fea0
nadia_lovely_29rpl <80080969+nadialvy@users.noreply.github.com>
2023-01-12 06:34:34 +0700

    Display data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import { useReducer } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchWeatherAction } from './redux/slices/weatherSlices';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 function App() {
-  
+  // dispatch action
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(fetchWeatherAction("London"))
+  },[dispatch]);
+
+  // select state from store
+  const state = useSelector((state)=>state);
+  console.log(state);
+
   return (
     <div className="p-10">
       <h1 className="text-5xl text-white text-center font-bold">Weather App</h1>
@@ -13,13 +24,26 @@ function App() {
         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div class="relative">
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg aria-hidden="true" class="w-5 h-5 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                <svg aria-hidden="true" class="w-5 h-5 dark:text-gray-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
-            {/* <input class="placeholder:italic placeholder:text-white block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/> */}
-            <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white bg-opacity-50 placeholder:text-black" placeholder="Search City, Area..." required />
+            <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white bg-opacity-50 placeholder:text-gray-900" placeholder="Search City, Area..." required />
             <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-800 hover:bg-blue-900 focus:ring-4 focus:outline-none font-medium rounded-md text-sm px-8 py-2 dark:focus:ring-blue-800">Search</button>
         </div>
       </form>
+
+      <div className="bg-white w-1/2 bg-opacity-50 mx-auto rounded-lg mt-10">
+        <div className="px-2 py-6">
+            <img
+              className="mx-auto"
+              alt='weather-icon'
+              src={`https://openweathermap.org/img/wn/${state.weather.weather[0].icon}@2x.png`}
+            />
+            <p className="text-lg font-bold text-gray-900 text-center capitalize">{state.weather.weather[0].description}</p>
+            <p className="text-5xl font-bold text-gray-900 text-center">{Math.round(state.weather.main.temp - 273.15)}°C</p>
+            <p className="text-lg font-bold text-gray-900 text-center">{state.weather.name}</p>
+            <p className="text-sm text-center mt-4 text-gray-800 px-2">The weather condition in {state.weather.name}, {" "} {state.weather?.sys?.country} is described as :{" "}{state.weather?.weather[0].description} with a temperature of{" "}{Math.round(state.weather.main.temp - 273.15)} °C and a humadity of {" "} {state.weather?.main?.humidity} %</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/redux/slices/weatherSlices.js
+++ src/redux/slices/weatherSlices.js
@@ -6,7 +6,8 @@ export const fetchWeatherAction = createAsyncThunk(
     'weather/fetch',
     async (payload, {rejectWithValue, getState, dispatch})=>{
         try {
-            const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`);
+            const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
+            console.log(data);
             return data
         } catch (error) {
             if(!error?.response){
