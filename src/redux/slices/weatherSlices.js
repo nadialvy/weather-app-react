7bfd9385f800cee2d339c797979fd01dfcd291db
nadia_lovely_29rpl <80080969+nadialvy@users.noreply.github.com>
2023-01-11 08:03:31 +0700

    get data with redux

---
+++ src/redux/slices/weatherSlices.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+//action
+export const fetchWeatherAction = createAsyncThunk(
+    'weather/fetch',
+    async (payload, {rejectWithValue, getState, dispatch})=>{
+        try {
+            const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`);
+            return data
+        } catch (error) {
+            if(!error?.response){
+                throw error
+            }
+            return rejectWithValue(error?.response?.data);
+        }
+    }
+);
+
+//slice
+const weatherSlice = createSlice({
+    name: "weather",
+    initialState: {data:"Loaded"},
+    extraReducers: (builder)=>{
+        //pending
+        builder.addCase(fetchWeatherAction.pending, (state, action)=>{
+            state.loading = true;
+        });
+        //fulfilled
+        builder.addCase(fetchWeatherAction.fulfilled, (state, action)=>{
+            state.weather = action?.payload;
+            state.loading = false;
+            state.error = undefined;
+        });
+        //rejected
+        builder.addCase(fetchWeatherAction.rejected, (state, action) => {
+            state.loading = false;
+            state.weather = undefined;
+            state.error = action?.payload
+        })
+    },
+})
+
+export default weatherSlice.reducer;
